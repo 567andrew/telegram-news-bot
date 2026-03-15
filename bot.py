07e055f5e85ed59b785e1485c7cee29f895853c4
Andrew567 <sl6267668134@gmail.com>
2026-03-16 03:02:49 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,17 @@
 from flask import Flask
 import requests
 import os
-import time
-import threading
 import feedparser
 
 TOKEN = "你的BOT_TOKEN"
-CHAT_ID = "你的CHAT_ID"
+CHAT_ID = "7502932042"
 
 WEBSITE = "https://telegram-news-bot-pdxd.onrender.com"
 
 app = Flask(__name__)
 
-sent_links = set()
-
 def send_message(text):
+
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
     response = requests.post(url, json={
@@ -22,26 +19,23 @@ def send_message(text):
         "text": text
     })
 
-    print("Telegram response:", response.text)
+    print("Telegram:", response.text)
+
 
 def fetch_news():
-    print("Checking news...")
+
+    print("Fetching news...")
 
     feeds = [
         "https://rss.cnn.com/rss/edition.rss",
-        "http://feeds.bbci.co.uk/news/rss.xml",
-        "https://www.reutersagency.com/feed/?best-topics=world&post_type=best"
+        "http://feeds.bbci.co.uk/news/rss.xml"
     ]
 
     for feed_url in feeds:
+
         feed = feedparser.parse(feed_url)
 
-        for entry in feed.entries[:5]:
-
-            if entry.link in sent_links:
-                continue
-
-            sent_links.add(entry.link)
+        for entry in feed.entries[:2]:
 
             message = f"""
 🌍 {entry.title}
@@ -54,30 +48,16 @@ Website: {WEBSITE}
 
             send_message(message)
 
-def news_loop():
-    while True:
-
-        try:
-            fetch_news()
-
-        except Exception as e:
-            print("Error:", e)
-
-        time.sleep(300)
 
 @app.route("/")
 def home():
-    return "News bot running"
 
-def start_news_thread():
+    fetch_news()
 
-    thread = threading.Thread(target=news_loop)
-    thread.daemon = True
-    thread.start()
+    return "News sent"
+
 
 if __name__ == "__main__":
-
-    start_news_thread()
 
     port = int(os.environ.get("PORT", 10000))
 
