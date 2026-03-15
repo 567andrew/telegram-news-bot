52e82a1916389c2503e69ad2dadf73173e011271
Andrew567 <sl6267668134@gmail.com>
2026-03-15 18:05:50 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,32 @@
 import time
-import sys
+import requests
 import os
+import sys
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import threading
+
+TOKEN = "8627523908:AAHF2jOi7YJLb-ckZZ_gQ9VVeMU_iSXbO30"
+CHAT_ID = "@world_monitor_news"
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": text
+    }
+    requests.post(url, data=data)
 
 print("Telegram News Bot started")
 
 def bot_loop():
+    send_message("🌍 World Monitor Bot Online\nRender server connected.")
     while True:
-        print("Bot running...")
-        sys.stdout.flush()
-        time.sleep(30)
+        try:
+            print("Bot running...")
+            sys.stdout.flush()
+            time.sleep(60)
+        except Exception as e:
+            print("Error:", e)
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
