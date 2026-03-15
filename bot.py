2113072151922b30546749a7731144f222345038
Andrew567 <sl6267668134@gmail.com>
2026-03-15 23:44:01 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,16 +2,12 @@ import os
 import requests
 import time
 from flask import Flask
-from threading import Thread
+import threading
 
 app = Flask(__name__)
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
-
-@app.route("/")
-def home():
-    return "bot running"
 
 def send_message(text):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
@@ -19,21 +15,21 @@ def send_message(text):
         "chat_id": CHAT_ID,
         "text": text
     }
-    r = requests.post(url, data=data)
-    print("SEND RESULT:", r.text)
+    requests.post(url, data=data)
 
-def loop():
+def news_loop():
     while True:
         print("BOT LOOP RUNNING")
         send_message("Andrew test message")
         time.sleep(60)
 
-def start():
-    t = Thread(target=loop)
-    t.start()
+@app.route("/")
+def home():
+    return "bot running"
 
 if __name__ == "__main__":
-    start()
+    thread = threading.Thread(target=news_loop)
+    thread.start()
 
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
