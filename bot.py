f097597eaf964450279a412faf1e6e28357c9d31
Andrew567 <sl6267668134@gmail.com>
2026-03-15 17:26:51 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,12 @@
 import time
+import sys
 
-print("Telegram News Bot running...")
+print("Telegram News Bot started")
 
 while True:
-    print("Bot working...")
-    time.sleep(60)
+    try:
+        print("Bot running...")
+        sys.stdout.flush()
+        time.sleep(30)
+    except Exception as e:
+        print("Error:", e)
