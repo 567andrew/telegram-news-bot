78e2b75c6d35f5ae17c1d19c37fc9fb1e6993b4d
Andrew567 <sl6267668134@gmail.com>
2026-03-23 11:13:17 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,7 @@
-import requests
-import os
 import time
 
-TOKEN = os.environ["BOT_TOKEN"]
-CHAT_ID = os.environ["CHAT_ID"]
+print("🚀 START SUCCESS")
 
-def send_test():
-    print("🚀 bot started")
-
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    data = {
-        "chat_id": CHAT_ID,
-        "text": "✅ 测试成功！机器人正常工作"
-    }
-
-    r = requests.post(url, data=data)
-    print("📨 status:", r.text)
-
-
-if __name__ == "__main__":
-    send_test()
-
-    while True:
-        print("😴 running...")
-        time.sleep(60)
+while True:
+    print("✅ RUNNING...")
+    time.sleep(10)
