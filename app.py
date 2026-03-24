226af2f7da1f4979183bb2bb69f004e3d0ffe77f
Andrew567 <sl6267668134@gmail.com>
2026-03-24 12:27:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-print("🔥 程序启动成功", flush=True)
+import time
+import requests
+import os
+
+def send(text):
+    TOKEN = os.environ["BOT_TOKEN"]
+    CHAT_ID = os.environ["CHAT_ID"]
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": CHAT_ID,
+        "text": text
+    }
+
+    requests.post(url, json=data)
+
+print("✅ 程序启动成功", flush=True)
 
 while True:
     try:
         print("🔄 正在运行...", flush=True)
+
         send("✅ Worker 正常运行")
+
     except Exception as e:
         print("❌ 错误:", e, flush=True)
 
